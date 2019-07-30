e5bd80e6bdf0d74bf64ad2ddf027aec76d0b768e
Ilya Murzinov <murz42@gmail.com>
2019-07-30 12:54:50 +0200

    update

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ window.onload = async function() {
     var month = document.getElementById("month").getElementsByTagName('input')[0].value;
     console.log(month);
 
+    if (month != "Juli") {
+        console.log("PREVIOUS!");
+        document.getElementById("labbevoreMonth").click();
+        return;
+    }
+
+    await sleep(5000);
+
     var cells = document.getElementsByClassName("CELL");
     var hasTermin = false;
     for (var i = 0; i < cells.length; i++) {
         var link = cells[i].getElementsByTagName('a')[0]
         if (link && link.style['text-decoration-line'] == 'underline') {
             hasTermin = true;
-            var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
-            audio.play();
-            alert("GOOOOO!!!!!!");
+            blink();
             break;
         }
     }
@@ -26,17 +32,21 @@ window.onload = async function() {
         return;
     }
 
-    await sleep(5000);
-
-    if (month != "Juli") {
-        console.log("PREVIOUS!");
-        document.getElementById("labbevoreMonth").click();
-    } else {
-        console.log("NEXT!");
-        document.getElementById("labnextMonth").click();
-    }
+    console.log("NEXT!");
+    document.getElementById("labnextMonth").click();
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}  
+}
+
+function blink() {
+    var body = document.getElementsByTagName('body')[0];
+    setInterval(function() {
+        if (body.style.background == "rgb(255, 0, 0)") {
+            body.style = "background: #F000000;"
+        } else {
+            body.style = "background: #F00;"
+        }
+    }, 250);
+}
